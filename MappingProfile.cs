8706be042217cf70ece6a296cde4acbdb8f52a7c
Shishikyuu <richard.febres@gmail.com>
2021-05-03 22:11:23 -0400

    Add post, put, delete functionality

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -11,6 +11,29 @@ namespace SchoolAPI
             CreateMap<Organization, OrganizationDto>()
                     .ForMember(c => c.FullAddress,
                         opt => opt.MapFrom(x => string.Join(' ', x.City, x.Country)));
+
+            CreateMap<OrganizationForCreationDto, Organization>();
+            CreateMap<OrganizationForUpdateDto, Organization>();
+
+            CreateMap<User, UserDto>();
+            CreateMap<UserForCreationDto, User>();
+            CreateMap<UserForUpdateDto, User>();
+
+            CreateMap<School, SchoolDto>();
+            CreateMap<SchoolForCreationDto, School>();
+            CreateMap<SchoolForUpdateDto, School>();
+
+            CreateMap<Assignment, AssignmentDto>();
+            CreateMap<AssignmentForCreationDto, Assignment>();
+            CreateMap<AssignmentForUpdateDto, Assignment>();
+
+            CreateMap<Section, SectionDto>();
+            CreateMap<SectionForCreationDto, Section>();
+            CreateMap<SectionForUpdateDto, Section>();
+
+            CreateMap<Submission, SubmissionDto>();
+            CreateMap<SubmissionForCreationDto, Submission>();
+            CreateMap<SubmissionForUpdateDto, Submission>();
         }
     }
 }
